e339b0a9c2f767295661602c90f8927ac0f2f34a
sumit viramgama <sumitviramgama1@gmail.com>
2024-12-20 10:03:52 +0530

    final pr1 commit

---
+++ pr_1_validstrin2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isValid(char str[]){
+    int len = strlen(str)-1;
+    if(len<3){
+        return false;
+    }
+    else if(len==3){
+        if(str[0]=='a' && str[1]=='b'&& str[2]=='b'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        for(int i=0;i<len-2;i++){
+            if(str[i] != 'a'){
+                return false;
+            }
+        }
+        for(int i=len-2;i<len;i++){
+            if(str[i]!='b'){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main()
+{
+   char str[10];
+   printf("Enter string : ");
+   fgets(str, 10 , stdin);
+   bool b=isValid(str);
+   if(b){
+       printf("String is valid\n");
+   }
+   else{
+       printf("string is invalid");
+   }
+
+   return 0;
+}
